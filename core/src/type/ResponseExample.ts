12685983d82a02c1782c7b425313b9c760b4a88b
Sören Klein <soerenklein98@gmail.com>
2026-05-18 09:07:44 +0200

    wip

--- core/src/type/ResponseExample.ts
+++ core/src/type/ResponseExample.ts
@@ -1,9 +1,11 @@
 import type {HttpStatusCode} from "./HttpStatusCode.ts";
+import type {Link} from "./Link.ts";
 
 export interface ResponseExample {
     httpStatusCode: HttpStatusCode;
     name: string | null;
     description: string;
+    links: Link[];
     body: {
         content: string;
         type: "plain" | "json";
